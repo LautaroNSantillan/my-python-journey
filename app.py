9deb94272c6c6dfa012188fea22e6fe508a65f69
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2024-06-16 18:36:29 -0300

    nested loops

--- app.py
+++ app.py
@@ -1,42 +1,55 @@
-choice = input("1 for guess game 2 for car game ")
+# choice = input("1 for guess game 2 for car game ")
+#
+# if choice == "1":
+#     secret_number = 9
+#     guess_count = 0
+#     guess_limit = 3
+#     while guess_count < guess_limit:
+#         guess = int(input("Guess the number: "))
+#         guess_count+=1
+#         if guess == secret_number:
+#             print("Done")
+#             break
+#     else:
+#         print("no more attempts")
+#
+# if choice == "2":
+#     command = ""
+#     started = False
+#     while True:
+#         command = input(">").lower()
+#         if command == "start":
+#             if started:
+#                 print("already started")
+#             else:
+#                 started=True
+#             print("car started")
+#         elif command == "stop":
+#             if not started:
+#                 print("already stopped")
+#             else:
+#                 started = False
+#             print("car stopped")
+#         elif command == "help":
+#             print("""
+# start
+# stop
+# quit
+#             """)
+#         elif command == "quit":
+#             break
+#         else:
+#             print("invalid command")
 
-if choice == "1":
-    secret_number = 9
-    guess_count = 0
-    guess_limit = 3
-    while guess_count < guess_limit:
-        guess = int(input("Guess the number: "))
-        guess_count+=1
-        if guess == secret_number:
-            print("Done")
-            break
-    else:
-        print("no more attempts")
+# FOR LOOP
+prices = [1,2,3]
+sum = 0
+for price in prices:
+    sum += price
+print(f"Total: {sum}")
 
-if choice == "2":
-    command = ""
-    started = False
-    while True:
-        command = input(">").lower()
-        if command == "start":
-            if started:
-                print("already started")
-            else:
-                started=True
-            print("car started")
-        elif command == "stop":
-            if not started:
-                print("already stopped")
-            else:
-                started = False
-            print("car stopped")
-        elif command == "help":
-            print("""
-start
-stop
-quit
-            """)
-        elif command == "quit":
-            break
-        else:
-            print("invalid command")
+# NESTED LOOP
+
+for x in range(4):  # excludes this digit
+    for y in range(2):
+        print(f"{x}, {y}")

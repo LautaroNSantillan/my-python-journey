f02e70f14e7eb2da2703b369cf5d923a2e156f00
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2024-06-21 12:56:57 -0300

    get,post

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def hello_world():
-    return "<h1>Hello World</h1>"
+    req_method = request.method
+    if req_method == 'POST':
+        first_name = request.form['first_name']
+        return redirect(url_for("name", first_name=first_name))
+    return render_template('req.html', request_method=req_method)
+
+
+@app.route('/name/<string:first_name>')
+def name(first_name):
+    return f'{first_name}'
 
 
 if __name__ == '__main__':
